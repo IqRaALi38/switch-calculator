fc20a29c49a863416c869308c74081466c0ac95c
IQRA AII <162632280+IqRaALi38@users.noreply.github.com>
2024-03-21 20:51:21 +0100

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const finalanswer :{
+  numberOne: number
+  numberTwo: number
+  operator: string
+} = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Enter first number",
+    }, {
+        type: "number",
+        name: "numberTwo",
+        message: "Enter second number",
+     },
+     {
+        type: "list",
+        name: "operator",
+        message: "select one of the operators to perform action",
+        choices: ["+", "-", "*", "/"],
+     },
+  ]);
+
+  const { numberOne, numberTwo, operator} = finalanswer;
+  let result 
+    switch (operator) {
+        case "+": 
+        result = numberOne + numberTwo; 
+            break;
+        case "-":
+            result = numberOne - numberTwo;
+            break;
+        case "*":
+            result = numberOne * numberTwo;
+            break;
+        case "/":
+            result = numberOne / numberTwo;
+            break;
+    
+        default:
+            console.log(chalk.red("Invalid operator"));
+    }
+    console.log(chalk.yellow(`${numberOne} ${operator} ${numberTwo} = ${result}`));
